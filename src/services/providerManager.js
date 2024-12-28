f34143c28937955c0fcba77da09c5c5dab58ca17
Andrés Cufari <andrescufari@Andress-MacBook-Pro-2.local>
2024-12-28 18:00:26 -0300

    fix

--- src/services/providerManager.js
+++ src/services/providerManager.js
@@ -1,5 +1,6 @@
 const { v4: uuidv4 } = require('uuid');
 const User = require('../models/userModel');
+const { ModelManager } = require('../config/models');
 
 class ProviderManager {
   constructor() {
